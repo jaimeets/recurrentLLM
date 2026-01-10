4b81d09f01ba504612fd540461813c5c3d5bd210
jaimeets <jaimeetsingh.ai@gmail.com>
2026-01-10 18:45:03 +0530

    states seperated from the module

--- model.py
+++ model.py
@@ -25,17 +25,16 @@ n_steps = 512
 class RNNCell(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()  
-        self.state = torch.zeros(hidden_size, device=device)
         self.w1 = nn.Linear(input_size, hidden_size)
         self.w2 = nn.Linear(hidden_size, hidden_size)
         self.w3 = nn.Linear(hidden_size, output_size)
-    
-    def forward(self, input):
+
+    def forward(self, input, state):
         out = self.w1(input)
-        self.state = self.w2(self.state)
-        self.state = F.tanh(out + self.state)
-        out = self.w3(self.state)
-        return out
+        state = self.w2(state)
+        state = F.tanh(out + state)
+        out = self.w3(state)
+        return out, state
     
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, n_cells):
@@ -47,20 +46,22 @@ class RNN(nn.Module):
         self.rnncells = [input_rnn] + self.rnncells + [output_rnn]
         self.rnncells = nn.ModuleList(self.rnncells)
 
-    def forward(self, idx):
+    def forward(self, idx, states):
         input = self.embed(idx)
-        for cell in self.rnncells:
-            input = cell(input)
-        return input
-    
-    def clear_state(self):
-        for cell in self.rnncells:
-            cell.state = torch.zeros_like(cell.state, device=device)
+        for i, cell in enumerate(self.rnncells):
+            input, states[i] = cell(input, states[i])
+        return input, states
 
 model = RNN(input_size, hidden_size, output_size, n_cells)
 model = model.to(device)
 # for name, param in model.named_parameters():
 #     print(name, param.shape, param.requires_grad)
+
+def collate_fn(batch):
+    for story in batch:
+        pass
+
+
 
 optimizer = torch.optim.Adam(
     model.parameters(),
@@ -72,15 +73,15 @@ optimizer = torch.optim.Adam(
 
 for step in range(1000):
     story = ds['train'][step]['text']
-    model.clear_state()
     optimizer.zero_grad()
+    states = [torch.zeros(hidden_size, device=device) for _ in range(n_cells)]
     total_loss = 0.0
     for idx, char in enumerate(story):
         if idx == len(story)-2:
             break
         char_id = torch.tensor(char_to_id[char], device=device)
         next_char = char_to_id[story[idx+1]]
-        out = model(char_id)
+        out, states = model(char_id, states)
         loss = F.cross_entropy(out, torch.tensor(next_char, device=device))   
         total_loss = total_loss + loss
 
@@ -107,3 +108,5 @@ for step in range(1000):
 #     fake += id_to_char[id]
 # print("this is fake")
 # print(fake)
+
+#fallback characters     
